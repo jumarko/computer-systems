0d9ad63b9e31d613fb916096f555cbf224d511dc
Juraj Martinka <jumarko@gmail.com>
2020-07-01 10:18:43 +0200

    Add sample main.c just for testing

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  07/01/2020 10:18:23
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Juraj Martinka (jumar), jumarko@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+int main() {
+  return 0;
+}
